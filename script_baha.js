b1e387bf3533d899aa878fd767918bf12ae5de6c
piyan69 <130072567+piyan69@users.noreply.github.com>
2025-08-17 21:59:10 +0800

    Update script_baha.js

--- script_baha.js
+++ script_baha.js
@@ -52,14 +52,44 @@ display_mode2.addEventListener('click', function() {
   const logo_frame = document.querySelector('.logo_frame');
   logo_frame.style.backgroundColor = "#2c2c2c";
   const hr = document.querySelector('hr');
-
-// 確保真的找到了 hr 元素
-if (hr) {
-  hr.style.border = 'none'; // 移除邊框
-  hr.style.height = '1px'; // 設定高度
-  hr.style.backgroundColor = "#2c2c2c"; // 現在背景色就生效了！
-}
+  // 確保真的找到了 hr 元素
+  if (hr) {
+    hr.style.border = 'none'; // 移除邊框
+    hr.style.height = '1px'; // 設定高度
+    hr.style.backgroundColor = "#2c2c2c"; // 現在背景色就生效了！
+  }
   
-
 }
 )
+const anime_container = document.querySelector('.anime_grid_container');
+for(let i=1;i<=15;i++){
+    let anime_card = document.createElement('div');
+    anime_card.classList.add('anime_card'+i);
+    anime_container.appendChild(anime_card);
+}
+let more_anime = document.createElement('div');
+more_anime.classList.add('more_anime');
+more_anime.style.cursor = "pointer";
+more_anime.style.position = "absolute";
+more_anime.style.left = "50%";
+more_anime.style.top ="100%";
+let arrow_down = document.createElement('img');
+arrow_down.src = "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRA24VpEsfbZ1ifCvYqvBB5xJtZJJkKRM-KoZmrENbaB8hfNAT2";
+arrow_down.style.width = "50px";
+arrow_down.style.height = "50px";
+arrow_down.style.borderRadius = "10px";
+
+more_anime.appendChild(arrow_down);
+anime_container.appendChild(more_anime);
+arrow_down.addEventListener('click', function() {
+    more_anime.style.display = "none";
+    // 這裡添加更多的動畫卡片
+    for(let i=16;i<=30;i++){
+        let anime_card = document.createElement('div');
+        anime_card.classList.add('anime_card'+i);
+        anime_container.appendChild(anime_card);
+    }
+    
+})
+
+
